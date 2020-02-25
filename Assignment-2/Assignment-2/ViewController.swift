bf067a4dd7b1e565721f1fa0760c0ec39a0717e8
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-02-24 21:22:53 -0600

    correcting repo with past assignments

---
+++ Assignment-2/Assignment-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-2
+//
+//  Created by Jessica Fenhouse on 2/13/20.
+//  Copyright © 2020 Jessica Fenhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
---
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Jessica Fenhouse on 2/6/20.
+//  Copyright © 2020 Jessica Fenhouse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
